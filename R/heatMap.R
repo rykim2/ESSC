3effd6671d92e169b23fd09f21b56f4a68d0c892
rykim2 <rykim2@usfca.edu>
2018-03-27 00:18:16 -0700

    Add files via upload
    Test with scholastic block result p-values.

--- R/heatMap.R
+++ R/heatMap.R
@@ -5,22 +5,23 @@ colnames(testMatrix) = paste(1:25)
 heatMap <- function(matrx, kmeans){
   library(gplots)
   library(dendextend)
-  # library()
   hc.rows <- as.dendrogram(hclust(dist(matrx)))
   hc.cols <- as.dendrogram(hclust(dist(t(matrx))))
   denRow <- color_branches(hc.rows, heat.colors(299), k = kmeans)
   denCol <- color_branches(hc.cols, heat.colors(299), k = kmeans)
-  HM <- heatmap.2(matrx, main = "Original Heatmap", col = cm.colors(dim(matrx)[2]), 
-                  trace = "none", density.info = "none", dendrogram = "none")
+  HM <- heatmap.2(matrx, main = "Original Heatmap", col = topo.colors(dim(matrx)[2]), 
+                  trace = "none", density.info = "none", dendrogram = "none", margins = c(5,5))
   HMRow <- heatmap.2(matrx[cutree(hc.rows, k = kmeans),], trace = "none", Rowv = denRow, 
-                     main = "Ordered by Rows", col = cm.colors(dim(matrx)[2]), dendrogram = "row")
+                     main = "Ordered by Rows", col = topo.colors(dim(matrx)[2]), dendrogram = "row")
   HMCol <- heatmap.2(matrx[,cutree(hc.cols, k = kmeans)], Colv = denCol, dendrogram = "column",
-                    main = "Ordered by Columns",col = cm.colors(dim(matrx)[2]), trace = "none")
+                    main = "Ordered by Columns",col = topo.colors(dim(matrx)[2]), trace = "none")
+  par(mfrow = c(3,1))
+
   return(c(HM, HMRow, HMCol))
 
 }
 
-heatMap(testMatrix, 4)
+heatMap(results.pois$PValues, 3)
 
 
 
@@ -28,8 +29,15 @@ heatMap(testMatrix, 4)
 
 
 
+# -------------------------------ROUGH DRAFT---------------------------------
+testMatrix <- results.pois$PValues
+hc.rows <- as.dendrogram(hclust(dist(testMatrix)))
+hc.cols <- as.dendrogram(hclust(dist(t(testMatrix))))
+denRow <- color_branches(hc.rows, heat.colors(299), k = 10)
+denCol <- color_branches(hc.cols, heat.colors(299), k = 3)
+HMCol <- heatmap.2(testMatrix[,cutree(hc.cols, k = 3)], Colv = denCol, dendrogram = "column",
+                   main = "Ordered by Columns",col = topo.colors(dim(testMatrix)[2]), trace = "none")
 
----------------------------##ROUGH DRAFT##-------------------------
 hc.rows <- hclust(dist(testMatrix))
 hc.cols <- hclust(dist(t(testMatrix)))
 heatmap.2(testMatrix, main = "Original Heatmap",  ColSideColors = rainbow(ncol(testMatrix)), RowSideColors = cc, col = c("blue", "green"), trace = "none")
