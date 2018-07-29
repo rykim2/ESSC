163ab4ad6a8c1ec0bc9eb48ce05f24ce751e05b1
rykim2 <rykim2@usfca.edu>
2018-07-29 01:11:40 -1000

    Final heatmap function

--- R/heatMap.R
+++ R/heatMap.R
@@ -1,67 +1,86 @@
-#Download ComplexHeatmap by:
-# library(devtools)
-# install_github("jokergoo/ComplexHeatmap")
+# Y label inputs. Graph Heatmap by levels
 
-#Set up: Load Matrix from ESSC 
-library(ESSC)
-net2 <- stochastic.block(n = 1000, k = 3, P = cbind(c(0.1, 0.01, 0.01), c(0.01, 0.1, 0.01), c(0.01, 0.01, 0.1)), sizes = c(300, 300, 400), random.community.assignment = TRUE)
-results.pois <- essc(net2$Adjacency, alpha = 0.10, Null = "Poisson")
-testMatrix <- results.pois$PValues
-
-#-----------------------------------Heatmap function-----------------------------#
-
-heatMap <- function(matrx, kRow, kCol){
+heatMap <- function(matrx, k, y = NULL){
   library(ComplexHeatmap)
   library(dendextend)
   library(circlize)
+  library(colorRamps)
   library(grid)
-  
-  # making a dendextend object to color my dendogram branches
-  hc.rows <- as.dendrogram(hclust(dist(matrx)))
-  hc.cols <- as.dendrogram(hclust(dist(t(matrx))))
-  denRow <- color_branches(hc.rows, heat.colors(299), k = kRow)
-  denCol <- color_branches(hc.cols, heat.colors(299), k = kCol)
-  
-  # Creating my ColorPalette and Lengend for Matrix Values
-  myPalt <- colorRamp2(c(0, 0.5, 1), c("lightyellow", "pink", "orange"))
-  lgd = Legend(at = c( 0, 0.5, 1), col_fun = myPalt, title = "Matrix", title_position = "topleft", grid_height = unit(10, "mm"))
-  
-  
-  # uses the Complex Heatmap object to create graphical objects --> GROB
-  HM <- Heatmap(matrx, column_title = "Original Heatmap", col = myPalt, 
-                show_row_dend = FALSE, show_column_dend = FALSE, show_heatmap_legend = FALSE)
-  
-  HMRow <- Heatmap(matrx, column_title = "Row Heatmap", col = myPalt, 
-                   show_column_dend = FALSE, cluster_rows = denRow, show_heatmap_legend = FALSE)
-  
-  HMCol <- Heatmap(matrx, column_title = "Column Heatmap", col = myPalt, 
-                   show_row_dend = FALSE, cluster_columns = denCol, show_heatmap_legend = FALSE)
+  library(stats)
 
-#------------------------------drawing Heatmap------------------------#
-grid.newpage()
-pushViewport(viewport(width = 0.9, height = 0.9, name = "base"))
+  y <- as.factor(y)
+  y <- sort(y, levels = y)
+  matrx <- matrx[sort(y),]
 
-seekViewport("base")
-pushViewport(viewport(width = 0.5, height = 0.5, just = c("right", "bottom"), name = "A"))
-draw(HM, newpage = FALSE)
+  myPalt <- colorRamp2(c(0, 0.5, 1), c("blue", "white", "purple"))
+  # myPalt <- structure(c("purple", "orange"), names = c("0", "1"))
 
-seekViewport("base")
-pushViewport(viewport(width = 0.5, height = 0.5, just = c("left", "bottom"), name = "B"))
-draw(HMRow, newpage = FALSE)
+  if(is.null(y) == TRUE){
 
-seekViewport("base")
-pushViewport(viewport(width = 0.5, height = 0.5, just = c("right", "top"), name = "C"))
-draw(HMCol, newpage = FALSE)
+    clust.rows <- hclust(dist(matrx))
+    hc.rows <- as.dendrogram(clust.rows)
 
-seekViewport("base")
-pushViewport(viewport(width = 0.5, height=0.5, just = c("left", "top"), name = "D"))
-grid.draw(lgd)
+    denRow <- color_branches(hc.rows, heat.colors(299), k = k)
 
-grab <- grid.grab()
+    HMRow <- Heatmap(matrx, name = "Matrix", col = myPalt, row_dend_reorder = TRUE,
+                     show_column_names = FALSE,
+                     show_column_dend = FALSE,
+                     show_heatmap_legend = TRUE,
+                     rect_gp = gpar(col = "white", lwd = 0.5),
+                     cluster_rows = denRow)
 
-return(grid.draw(grab))
+    draw(HMRow, newpage = TRUE)
 
+  }
+
+  else{
+    try(expr = if(length(y) != nrow(matrx)) stop("length of response vector y must be the same as the number of observations", call. = FALSE))
+
+    if(length(y) == nrow(matrx)){
+      ## Associating response y and reordering accordingly  ##
+      lev <- length(levels(y))
+
+      ## HEATMAP ANNOTATION : creating a named- vector
+      colrs <- palette(matlab.like(n = lev))
+      named.vec <- c(structure(colrs, names=levels(y)))
+
+      ha <- rowAnnotation(df = data.frame(labs = y), name = "truth",
+                              width = unit(0.5,"cm"), show_legend = TRUE,
+                              col = list(labs = c(named.vec)))
+
+      ## starting HeatMap function
+      # making a dendextend object to color my dendogram branches
+      clust.rows <- hclust(dist(matrx))
+
+      hc.rows <- as.dendrogram(clust.rows)
+      denRow <- color_branches(hc.rows, heat.colors(299), k = k)
+
+      HM <- Heatmap(matrix = matrx, name = "Matrix", col = myPalt, row_dend_reorder = FALSE, show_column_names = FALSE,
+                       show_column_dend = FALSE, rect_gp = gpar(col = "white", lwd = 0.5),
+                       cluster_rows = denRow, gap = unit(2, "mm"))
+
+      draw(ha + HM, newpage = TRUE)
+      return(list(Node_Clusters = clust.rows))
+
+    }
+  }
 }
 
-heatMap(testMatrix, kRow = 500, kCol = 3)
+heatMap(matrx = testMatrix.FB, y = y, k = 8)
 
+##TESTING MY FUNCTION
+library(ESSC)
+library(igraph)
+FB_graph <- graph_from_edgelist(as.matrix(FB_edgelist[, 1:2]), directed = FALSE)
+FB_adjacency <- get.adjacency(FB_graph)
+
+results.FB <- essc(FB_adjacency, alpha = 0.10, Null = "Poisson")
+testMatrix.FB <- results.FB$Indicator_Matrix
+
+y <- FB_labels$Location
+matrx <- results.FB$PValues
+
+# new matrix sorted with 1 or 0
+# y <- as.factor(y)
+# y <- sort(y, levels = y)
+# matrx2 <- matrx[sort(y),]
